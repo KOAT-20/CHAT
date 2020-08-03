3aa2b58e8ded463fdac61454a8776e60db58b93b
Kevin <keviinnalvarez2015@gmail.com>
2020-08-03 14:43:33 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 export default function App () {
   return (
     <div>
-        <img src={logo} className="App-logo" alt="logo" />
+        hola
     </div>
   );
 }
