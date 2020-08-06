10bc0846194474a660be82a695a0ae8b28378530
Kevin <keviinnalvarez2015@gmail.com>
2020-08-06 12:41:13 -0400

    preparando rutas

--- back/index.js
+++ back/index.js
@@ -4,7 +4,8 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const router = require('./router');
 
 const PORT = process.env.PORT || 5000;
-
+app.use(router);
 server.listen(PORT, () => console.log(`Servidor inciado en el puerto ${PORT}`));
