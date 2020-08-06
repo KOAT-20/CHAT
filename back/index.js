120f90f716a32d81932d1cb9d95446fdc43c4134
Kevin <keviinnalvarez2015@gmail.com>
2020-08-06 15:23:15 -0400

    configurando el envio de mensajes

--- back/index.js
+++ back/index.js
@@ -6,15 +6,28 @@ const server = http.createServer(app);
 const io = socketio(server);
 const router = require('./router');
 
+const { addUser, deleteUser, getUser, getUsersRoom } = require('./aux/users.js');
+
 const PORT = process.env.PORT || 5000;
 app.use(router);
 server.listen(PORT, () => console.log(`Servidor inciado en el puerto ${PORT}`));
 
 io.on('connection', (socket) => {
-  console.log('Conexión socket');
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({id: socket.id, name, room});
+    if (error) return callback(error);
+    socket.emit('message', {user: 'admin', text: `${user.name} Bienvenido a la sala ${user.room}`});
+    socket.brodcast.to(user.room).emit('message', {user: 'admin', text:`${user.name}, se ha unido!`})
+    socket.join(user.room);
+    callback();
   });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit('message', {user: user.name, text: message});
+    callback();
+  });
+
   socket.on('disconnect', () => {
     console.log('Desconexión socket');
   })
