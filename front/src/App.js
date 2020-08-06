2912cfeb715abf25e4a367269db96956906bf000
Kevin <keviinnalvarez2015@gmail.com>
2020-08-06 12:20:43 -0400

    comienzo

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+/* components */
+import Chat from './components/Chat';
+import Join from './components/Join';
 
 export default function App () {
   return (
-    <div>
-        hola
-    </div>
+    <Router>
+      <Route path='/' exact component={Join} />
+      <Route path='/chat' component={Chat} />
+    </Router>
   );
 }
