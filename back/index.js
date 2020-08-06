fbc6adc94c0b25f2b8754e2850c76b2a6f31a3e2
Kevin <keviinnalvarez2015@gmail.com>
2020-08-06 12:28:26 -0400

    servidor configurado

---
+++ back/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Servidor inciado en el puerto ${PORT}`));
