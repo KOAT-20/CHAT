29841eeb538bbf1fe103d8e7ae06d198eeed5098
Kevin <keviinnalvarez2015@gmail.com>
2020-08-06 13:40:49 -0400

    Config Join

--- front/src/components/Join/index.js
+++ front/src/components/Join/index.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Container, ContainerInner, Title, Input, Button
+} from './join.js';
 
 export default function Join () {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
   return (
-    <div>Join</div>
+    <Container>
+      <ContainerInner>
+        <Title>Join</Title>
+        <Input type='text' placeholder='Name...' onChange={(event) => setName(event.target.value)} />
+        <Input type='text' placeholder='Room...' onChange={(event) => setRoom(event.target.value)} />
+        <Link
+          onClick={event => (!name || !room) ? event.preventDefault() : null}
+          to={`/chat?name=${name}&room=${room}`}
+          >
+          <Button type='submit'>Sign In</Button>
+        </Link>
+      </ContainerInner>
+    </Container>
   );
 }
---
+++ front/src/components/Join/join.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    height: 100vh;
+    align-items: center;
+    background-color: #1A1A1D;
+`;
+
+export const ContainerInner = styled.div`
+    width: 20%;
+`;
+
+export const Title = styled.h1`
+    color: white;
+    font-size: 2.5em;
+    padding-bottom: 10px;
+    border-bottom: 2px solid white;
+`;
+
+export const Input = styled.input`
+    margin-top: 5px;
+    border-radius: 10px;
+    padding: 15px 20px;
+    width: 85%;
+`;
+
+export const Button = styled.button`
+    cursor: pointer;
+    color: #fff !important;
+    text-transform: uppercase;
+    text-decoration: none;
+    background: #2979FF;
+    padding: 20px;
+    border-radius: 10px;
+    display: inline-block;
+    border: none;
+    width: 100%;
+    margin-top: 5px;
+`;
